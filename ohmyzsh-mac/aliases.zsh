bfe93c243d901139733400f228108d973eaf5325
Viranch Mehta <viranch.mehta@gmail.com>
2012-11-05 16:18:14 +0530

    Replace way2sms script with ontextme script

--- ohmyzsh-mac/aliases.zsh
+++ ohmyzsh-mac/aliases.zsh
@@ -6,6 +6,6 @@ SCRIPTS="$HOME/playground/scripts"
 alias imdb='p $SCRIPTS/imdb.py'
 alias usage='sh $SCRIPTS/broadband_usage.sh'
 alias skins-dl='p $SCRIPTS/skins-dl.py'
-alias text='p $SCRIPTS/way2sms.py 9374394249'
+alias text='p $SCRIPTS/ontextme.py 9374394249'
 alias pb='p $SCRIPTS/pb.py'
 alias ko='p $SCRIPTS/qt-open.py'
