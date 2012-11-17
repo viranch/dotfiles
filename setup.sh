20af1cb8435c89d49129f162dc9b251b20381275
Viranch Mehta <viranch.mehta@gmail.com>
2012-11-17 19:16:13 +0530

    Add aws setup stuff

--- setup.sh
+++ setup.sh
@@ -16,3 +16,15 @@ if [[ "$platform" == "Linux" ]]; then
 elif [[ "$platform" == "Darwin" ]]; then
     rm -f ~/.zshrc && ln -s $cwd/ohmyzsh-mac/zshrc ~/.zshrc
 fi
+
+# AWS-specific configuration
+if [[ "$(hostname)" =~ ^ip- ]]; then
+    mkdir -p ~/.config/transmission-daemon
+    for file in /etc/httpd/conf/httpd.conf /etc/ssh/sshd_config ~/.config/transmission-daemon/settings.json; do
+        sudo rm -f $file && sudo ln -s $cwd/aws_`basename $file` $file
+    done
+    if [[ -L /srv/http/stuff ]]; then
+        mkdir -p ~/Downloads
+        sudo ln -s ~/Downloads /srv/http/stuff
+    fi
+fi
