308eba2e0a12a9d95e51869477c5299aceb65574
Viranch Mehta <viranch.mehta@gmail.com>
2013-04-26 23:42:54 +0530

    Make extract function non-verbose

--- ohmyzsh-common/functions.zsh
+++ ohmyzsh-common/functions.zsh
@@ -11,16 +11,16 @@ function extract() {
     fi
   if [[ -f $1 ]]; then
     case $1 in
-      *.tar.bz2) tar xvjf $1;;
-      *.tar.gz) tar xvzf $1;;
-      *.tar.xz) tar xvJf $1;;
-      *.tar.lzma) tar --lzma -xvf $1;;
+      *.tar.bz2) tar xjf $1;;
+      *.tar.gz) tar xzf $1;;
+      *.tar.xz) tar xJf $1;;
+      *.tar.lzma) tar --lzma -xf $1;;
       *.bz2) bunzip $1;;
       *.rar) unrar x $1;;
       *.gz) gunzip $1;;
-      *.tar) tar xvf $1;;
-      *.tbz2) tar xvjf $1;;
-      *.tgz) tar xvzf $1;;
+      *.tar) tar xf $1;;
+      *.tbz2) tar xjf $1;;
+      *.tgz) tar xzf $1;;
       *.zip) unzip $1;;
       *.Z) uncompress $1;;
       *.7z) 7z x $1;;
