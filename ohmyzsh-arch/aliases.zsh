629554eb9fe1ffa896b53ae663da9bf37e7e2fac
Viranch Mehta <viranch.mehta@gmail.com>
2013-10-20 15:26:08 +0530

    zsh arch- add alias to list packages installed from AUR

--- ohmyzsh-arch/aliases.zsh
+++ ohmyzsh-arch/aliases.zsh
@@ -60,6 +60,7 @@ alias pql='pq -l'
 alias pqo='pq -o'
 alias pqi='pq -i'
 alias pr='P -Rs'
+alias aur='pq -m'
 whose () { pqo -q $(which $1) }
 plist() { pacman -Qei|awk ' BEGIN {FS=":"}/^Name/{printf("\033[1;36m%s\033[1;37m", $2)}/^Description/{print $2}' }
 compdef _which whose=which
