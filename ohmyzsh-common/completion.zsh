6e19d0e014e8fb4ab12e0b29bcc12efe04b4d851
Viranch Mehta <viranch.mehta@gmail.com>
2014-05-20 00:19:48 +0530

    zsh common- show dots while processing completion

--- ohmyzsh-common/completion.zsh
+++ ohmyzsh-common/completion.zsh
@@ -64,6 +64,14 @@ zstyle ':completion:*:*:*:users' ignored-patterns \
 # ... unless we really want to.
 zstyle '*' single-ignored show
 
+expand-or-complete-with-dots() {
+    echo -n "\e[31m...\e[0m"
+    zle expand-or-complete
+    zle redisplay
+}
+zle -N expand-or-complete-with-dots
+bindkey "^I" expand-or-complete-with-dots
+
 compdef _sudo fucking=sudo
 
 # kill stuff
