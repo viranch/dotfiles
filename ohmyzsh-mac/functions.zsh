d68fd452cd500f90ba87c0885f7b44ab48cc6204
Viranch Mehta <viranch.mehta@gmail.com>
2014-08-17 11:54:09 +0530

    zsh mac- add mac specific 'share' function

---
+++ ohmyzsh-mac/functions.zsh
@@ -0,0 +1,13 @@
+function share() {
+    temp="/tmp/myshare-$RANDOM" 
+    mkdir -p $temp
+    for f in "$@"; do
+        ln -s "$(realpath $f)" "$temp/$(basename $f)"
+    done
+    cd $temp
+    ip=`ifconfig en0 | grep "inet " | awk '{print $2}'`
+    echo "Starting server at http://$ip:8000/ ..."
+    python -m SimpleHTTPServer 2> /dev/null
+    cd -
+    rm -rf $temp
+}
