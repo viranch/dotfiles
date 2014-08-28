571a346af37faf3b3fa914610f4421645934c0ae
Viranch Mehta <viranch.mehta@gmail.com>
2014-08-28 11:47:55 +0530

    zsh common git- add function to generate link to create new merge request in gitlab and open the link in browser

--- ohmyzsh-common/git.zsh
+++ ohmyzsh-common/git.zsh
@@ -167,5 +167,15 @@ function gllink() {
     echo $link
 }
 
+function mr() {
+    local token="ypg3dxF8Ag6vdx8pNNQW"
+    local link="$(git remote -v | head -n1 | awk '{print $2}')"
+    local project="$(echo $link | cut -d':' -f2 | sed 's/\.git$//g')"
+    local base="https://$(echo $link | cut -d':' -f1 | cut -d'@' -f2)"
+    local project_id="$(curl -sk -H "PRIVATE-TOKEN: $token" $base/api/v3/projects | ruby -e "require 'json'; puts (JSON.parse(STDIN.read).find{|p| p['path_with_namespace'] == '$project'})['id']")"
+    local req="$base/$project/merge_requests/new?utf8=%E2%9C%93&merge_request%5Bsource_project_id%5D=$project_id&merge_request%5Bsource_branch%5D=`current_branch`&merge_request%5Btarget_project_id%5D=$project_id&merge_request%5Btarget_branch%5D=master"
+    test -x /usr/bin/xdg-open && xdg-open $req || echo $req
+}
+
 alias gconf='git config -e'
 compdef _git gconf='git-config'
