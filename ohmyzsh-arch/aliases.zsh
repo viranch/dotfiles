98ce5c160b6b926dccf7cddc3e5a5cca1b46ea0a
Viranch Mehta <viranch.mehta@gmail.com>
2014-08-29 19:08:37 +0530

    zsh arch- add aliases to turn on/off one monitor to the right of laptop

--- ohmyzsh-arch/aliases.zsh
+++ ohmyzsh-arch/aliases.zsh
@@ -26,6 +26,8 @@ alias sys-reload='sys daemon-reload'
 alias monitor='xranr --output VGA1'
 alias onmon='xrandr --output VGA1 --auto && xrandr --output LVDS1 --off && test -f ~/.fehbg && . ~/.fehbg'
 alias offmon='xrandr --output LVDS1 --auto && xrandr --output VGA1 --off && test -f ~/.fehbg && . ~/.fehbg'
+alias onmon1='xrandr --output VGA1 --auto --right-of LVDS1 && test -f ~/.fehbg && . ~/.fehbg'
+alias offmon1='xrandr --output VGA1 --off'
 alias onmons='xrandr --output DP1 --auto && xrandr --output LVDS1 --off && xrandr --output VGA1 --auto --right-of DP1 && test -f ~/.fehbg && . ~/.fehbg'
 alias offmons='xrandr --output DP1 --off && xrandr --output LVDS1 --auto && xrandr --output VGA1 --off'
 # aws
