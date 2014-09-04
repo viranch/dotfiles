a6a1c9aea37915cc6467671312068321cea6dc87
Viranch Mehta <viranch.mehta@gmail.com>
2014-09-04 11:46:10 +0530

    zsh common- add function to generate and open link to create new issue in gitlab

--- ohmyzsh-common/gitlab.zsh
+++ ohmyzsh-common/gitlab.zsh
@@ -8,9 +8,17 @@ function gllink() {
     echo $link
 }
 
+# Generate gitlab web link to create new issue
+# and open it with xdg-open
+function new_issue() {
+    local link="$(git remote -v | head -n1 | awk '{print $2}' | sed 's/:/\//g' | sed 's/git@/https:\/\//g' | sed 's/\.git$//g')/issues/new"
+    test -x /usr/bin/xdg-open && xdg-open $link || echo $link
+}
+alias ni=new_issue
+
 # Generate gitlab web link to create new merge request for current branch
 # and open the link with xdg-open
-function mr() {
+function merge_request() {
     local token="ypg3dxF8Ag6vdx8pNNQW"
     local link="$(git remote -v | head -n1 | awk '{print $2}')"
     local project="$(echo $link | cut -d':' -f2 | sed 's/\.git$//g')"
@@ -19,3 +27,4 @@ function mr() {
     local req="$base/$project/merge_requests/new?utf8=%E2%9C%93&merge_request%5Bsource_project_id%5D=$project_id&merge_request%5Bsource_branch%5D=`current_branch`&merge_request%5Btarget_project_id%5D=$project_id&merge_request%5Btarget_branch%5D=master"
     test -x /usr/bin/xdg-open && xdg-open $req || echo $req
 }
+alias mr=merge_request
