87fa5ffe35d1aef900081391c34f74bece0d764b
Viranch Mehta <viranch.mehta@gmail.com>
2014-09-04 13:48:08 +0530

    zsh common- project_id is not required to create merge requests in gitlab, remove the same

--- ohmyzsh-common/gitlab.zsh
+++ ohmyzsh-common/gitlab.zsh
@@ -19,12 +19,8 @@ alias ni=new_issue
 # Generate gitlab web link to create new merge request for current branch
 # and open the link with xdg-open
 function merge_request() {
-    local token="ypg3dxF8Ag6vdx8pNNQW"
-    local link="$(git remote -v | head -n1 | awk '{print $2}')"
-    local project="$(echo $link | cut -d':' -f2 | sed 's/\.git$//g')"
-    local base="https://$(echo $link | cut -d':' -f1 | cut -d'@' -f2)"
-    local project_id="$(curl -sk -H "PRIVATE-TOKEN: $token" $base/api/v3/projects | ruby -e "require 'json'; puts (JSON.parse(STDIN.read).find{|p| p['path_with_namespace'] == '$project'})['id']")"
-    local req="$base/$project/merge_requests/new?utf8=%E2%9C%93&merge_request%5Bsource_project_id%5D=$project_id&merge_request%5Bsource_branch%5D=`current_branch`&merge_request%5Btarget_project_id%5D=$project_id&merge_request%5Btarget_branch%5D=master"
-    test -x /usr/bin/xdg-open && xdg-open $req || echo $req
+    local base="$(git remote -v | head -n1 | awk '{print $2}' | sed 's/:/\//g' | sed 's/git@/https:\/\//g' | sed 's/\.git$//g')"
+    local link="$base/merge_requests/new?utf8=%E2%9C%93&merge_request%5Bsource_branch%5D=`current_branch`&merge_request%5Btarget_branch%5D=master"
+    test -x /usr/bin/xdg-open && xdg-open $link || echo $link
 }
 alias mr=merge_request
