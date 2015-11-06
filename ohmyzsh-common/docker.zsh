f328525344f56da3c358993a578c7e7687ca8e3b
Viranch Mehta <viranch.mehta@gmail.com>
2015-11-06 19:36:34 +0530

    zsh common- add aliases for docker

---
+++ ohmyzsh-common/docker.zsh
@@ -0,0 +1,18 @@
+alias dc=docker
+
+alias db='docker build -t'
+alias dp='docker pull'
+alias dim='docker images'
+
+alias drm='docker run --rm -it'
+
+alias dps='docker ps'
+alias dpsa='docker ps -a'
+alias de='docker exec -it'
+alias dl='docker logs -f'
+alias din='docker inspect'
+
+alias dsa='docker start'
+alias dst='docker stop'
+alias dre='docker restart'
+alias drm='docker rm'
