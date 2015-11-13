f569e051066bea15ed23a86e72474ca55e1bda30
Viranch Mehta <viranch.mehta@gmail.com>
2015-11-13 13:05:38 +0530

    zsh- move docker machine & vbox stuff to common; it will auto-detect existance of them and setup environment

--- ohmyzsh-common/aliases.zsh
+++ ohmyzsh-common/aliases.zsh
@@ -75,6 +75,22 @@ alias zshrc='vim ~/.zshrc && . ~/.zshrc'
 alias gitconfig='vim ~/.gitconfig'
 alias sshconfig='vim ~/.ssh/config'
 alias sz='. ~/.zshrc'
+# docker
+alias dc=docker
+alias db='docker build -t'
+alias dp='docker pull'
+alias dim='docker images'
+alias drmi='docker rmi'
+alias drn='docker run --rm -it'
+alias dps='docker ps'
+alias dpsa='docker ps -a'
+alias de='docker exec -it'
+alias dl='docker logs -f'
+alias din='docker inspect'
+alias dsa='docker start'
+alias dst='docker stop'
+alias dre='docker restart'
+alias drm='docker rm'
 
 function chpwd() {
   emulate -L zsh
--- ohmyzsh-common/docker.zsh
+++ ohmyzsh-common/docker.zsh
@@ -1,19 +1,87 @@
-alias dc=docker
+VM=default
+DOCKER_MACHINE=$(command -v docker-machine)
+VBOXMANAGE=$(command -v VBoxManage)
 
-alias db='docker build -t'
-alias dp='docker pull'
-alias dim='docker images'
-alias drmi='docker rmi'
+#unset DYLD_LIBRARY_PATH
+#unset LD_LIBRARY_PATH
 
-alias drn='docker run --rm -it'
+function vm_info() {
+    test -z "$DOCKER_MACHINE" && (echo "Docker Machine is not installed."; return 2)
+    test -z "$VBOXMANAGE" && (echo "VirtualBox is not installed."; return 2)
 
-alias dps='docker ps'
-alias dpsa='docker ps -a'
-alias de='docker exec -it'
-alias dl='docker logs -f'
-alias din='docker inspect'
+    $VBOXMANAGE showvminfo $VM &> /dev/null
+}
 
-alias dsa='docker start'
-alias dst='docker stop'
-alias dre='docker restart'
-alias drm='docker rm'
+function start_docker() {
+    BLUE='\033[0;34m'
+    GREEN='\033[0;32m'
+    NC='\033[0m'
+
+    vm_info
+    VM_EXISTS=$?
+    if [ $VM_EXISTS -eq 2 ]; then
+        return 1
+    elif [ $VM_EXISTS -eq 1 ]; then
+        echo "Creating Machine $VM..."
+        $DOCKER_MACHINE rm -f $VM &> /dev/null
+        rm -rf ~/.docker/machine/machines/$VM
+        $DOCKER_MACHINE create -d virtualbox --virtualbox-memory 2048 --virtualbox-disk-size 204800 $VM
+    else
+        echo "Machine $VM already exists in VirtualBox."
+    fi
+
+    VM_STATUS=$($DOCKER_MACHINE status $VM)
+    if [ "$VM_STATUS" != "Running" ]; then
+        echo "Starting machine $VM..."
+        $DOCKER_MACHINE start $VM
+    fi
+
+    echo "Setting environment variables for machine $VM..."
+    setup_env
+
+    cat << EOF
+
+
+                        ##         .
+                  ## ## ##        ==
+               ## ## ## ## ##    ===
+           /"""""""""""""""""\___/ ===
+      ~~~ {~~ ~~~~ ~~~ ~~~~ ~~~ ~ /  ===- ~~~
+           \______ o           __/
+             \    \         __/
+              \____\_______/
+
+
+EOF
+    echo -e "${BLUE}docker${NC} is configured to use the ${GREEN}$VM${NC} machine with IP ${GREEN}$($DOCKER_MACHINE ip $VM)${NC}"
+    echo "For help getting started, check out the docs at https://docs.docker.com"
+    echo
+}
+
+# setup environment for docker
+function setup_env() {
+    eval $($DOCKER_MACHINE env $VM --shell=zsh)
+}
+
+# test debian wheezy image with updated package list for
+# running test commands (eg: searching for packages)
+function deb() {
+    image=$(docker images | grep "^deb\s\+")
+    if [[ -z "$image" ]]; then
+        mkdir -p /tmp/deb
+        cat << EOF > /tmp/deb/Dockerfile
+FROM debian:wheezy
+RUN apt-get update
+EOF
+        docker build -t deb /tmp/deb
+        rm -rf /tmp/deb
+    fi
+    docker run --rm -it deb $@
+}
+
+# search for packages using the test debian container
+function apt-search {
+    deb apt-cache search $@
+}
+
+vm_info >/dev/null && [[ "$($DOCKER_MACHINE status $VM)" == "Running" ]] && setup_env
