b660a2d545f5f9530e1d692dfad4e629386acc4c
Viranch Mehta <viranch.mehta@gmail.com>
2015-11-22 22:40:30 +0530

    zsh mac- fix ls coloring for dark backgrounds

--- ohmyzsh-mac/theme-and-appearance.zsh
+++ ohmyzsh-mac/theme-and-appearance.zsh
@@ -3,6 +3,7 @@ autoload colors; colors;
 
 # Find the option for using colors in ls, depending on the version: Linux or BSD
 ls --color -d . &>/dev/null 2>&1 && alias ls='ls --color=tty' || alias ls='ls -G'
+export LSCOLORS=gxBxhxDxfxhxhxhxhxcxcx
 
 #setopt no_beep
 setopt auto_cd
