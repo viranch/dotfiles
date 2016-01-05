73082348ca60dd94d58d108aabcdac26233c449e
Viranch Mehta <viranch.mehta@gmail.com>
2016-01-05 10:34:55 +0530

    zsh mac- reimplement du wrapper to use gsort that supports human readable size for sorting

--- ohmyzsh-mac/aliases.zsh
+++ ohmyzsh-mac/aliases.zsh
@@ -5,6 +5,7 @@ alias or='open -R'
 alias sv='sudo vim'
 alias ns='sudo netstat -nap tcp'
 alias ipp='ifconfig en0 | grep "inet " | awk "{print \$2}"'
+du() { /usr/bin/du -sh $@ | gsort -rhk 1 } # install gsort with brew install coreutils
 
 # home brew
 alias b='brew'
