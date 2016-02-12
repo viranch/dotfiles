2f2a45639151a55480c9c79ce863aed484d9a88d
Viranch Mehta <viranch.mehta@gmail.com>
2016-02-12 11:36:00 +0530

    zsh common- whitespace+indentation fix

--- ohmyzsh-common/functions.zsh
+++ ohmyzsh-common/functions.zsh
@@ -4,37 +4,37 @@ function zsh_stats() {
 
 function extract() {
     unset REMOVE_ARCHIVE
-    
+
     if test "$1" = "-r"; then
         REMOVE=1
         shift
     fi
-  if [[ -f $1 ]]; then
-    case $1 in
-      *.tar.bz2) tar xjf $1;;
-      *.tar.gz) tar xzf $1;;
-      *.tar.xz) tar xJf $1;;
-      *.tar.lzma) tar --lzma -xf $1;;
-      *.bz2) bunzip $1;;
-      *.rar) unrar x $1;;
-      *.gz) gunzip $1;;
-      *.tar) tar xf $1;;
-      *.tbz2) tar xjf $1;;
-      *.tgz) tar xzf $1;;
-      *.zip) unzip $1;;
-      *.Z) uncompress $1;;
-      *.7z) 7z x $1;;
-      *) echo "'$1' cannot be extracted via >extract<";;
-    esac
+    if [[ -f $1 ]]; then
+        case $1 in
+          *.tar.bz2) tar xjf $1;;
+          *.tar.gz) tar xzf $1;;
+          *.tar.xz) tar xJf $1;;
+          *.tar.lzma) tar --lzma -xf $1;;
+          *.bz2) bunzip $1;;
+          *.rar) unrar x $1;;
+          *.gz) gunzip $1;;
+          *.tar) tar xf $1;;
+          *.tbz2) tar xjf $1;;
+          *.tgz) tar xzf $1;;
+          *.zip) unzip $1;;
+          *.Z) uncompress $1;;
+          *.7z) 7z x $1;;
+          *) echo "'$1' cannot be extracted via >extract<";;
+        esac
 
-    if [[ $REMOVE_ARCHIVE -eq 1 ]]; then
-        echo removing "$1";
-        /bin/rm "$1";
+        if [[ $REMOVE_ARCHIVE -eq 1 ]]; then
+            echo removing "$1";
+            /bin/rm "$1";
+        fi
+
+    else
+        echo "'$1' is not a valid file"
     fi
-
-  else
-    echo "'$1' is not a valid file"
-  fi
 }
 
 function kill-ssh() {
