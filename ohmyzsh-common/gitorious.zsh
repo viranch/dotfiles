3d4f917906f00d79e1c8cfaaa65f00405d70385b
Viranch Mehta <viranch.mehta@gmail.com>
2016-03-18 14:54:52 -0700

    zsh common- drop '.git' from output of gitorious commit link

--- ohmyzsh-common/gitorious.zsh
+++ ohmyzsh-common/gitorious.zsh
@@ -4,7 +4,7 @@ function gslink() {
     test -z "$chash" && chash=`git log --pretty=format:"%h" | head -n1`
     local gremote="$(git remote -v | head -n1)"
     local remote=`echo $gremote | grep -o "@.*:" | sed 's/[@:]//g'`
-    local repo=`echo $gremote | grep -o ":.*\.git" | sed 's/://g'`
+    local repo=`echo $gremote | grep -o ":.*\.git" | sed 's/:\(.*\)\.git/\1/g'`
     local link="https://$remote/$repo/commit/$chash"
     test -x /usr/bin/xsel && echo $link | xsel
     test -x /usr/bin/xclip && echo $link | xclip
