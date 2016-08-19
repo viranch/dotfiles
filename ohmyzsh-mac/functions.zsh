57e293f7bef017f422e869b92bb0ba6d1101ebf9
Viranch Mehta <email@viranch.me>
2016-08-19 12:32:59 +0530

    zsh mac- yubikey magic

--- ohmyzsh-mac/functions.zsh
+++ ohmyzsh-mac/functions.zsh
@@ -1,5 +1,5 @@
 function share() {
-    temp="/tmp/myshare-$RANDOM" 
+    temp="/tmp/myshare-$RANDOM"
     mkdir -p $temp
     for f in "$@"; do
         ln -s "$(realpath $f)" "$temp/$(basename $f)"
@@ -10,6 +10,21 @@ function share() {
     python -m SimpleHTTPServer 2> /dev/null
     cd -
     rm -rf $temp
+}
+
+function do_yubikey() {
+    osascript -e 'tell application "yubiswitch" to KeyOn'
+    [[ ! -f /tmp/yubi ]] && (echo 0 > /tmp/yubi)
+    n=`cat /tmp/yubi`
+    echo $n+1 | bc > /tmp/yubi
+    sleep 15
+    n=`cat /tmp/yubi`
+    [[ $n == "1" ]] && osascript -e 'tell application "yubiswitch" to KeyOff'
+    echo $n-1 | bc > /tmp/yubi
+}
+
+function listen_yubikey() {
+    while true; do nc -l 11000; (do_yubikey &); done
 }
 
 unalias ff
