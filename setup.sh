2ae8c97ff7120297c289e7938ce776214afb5d13
Viranch Mehta <email@viranch.me>
2017-10-09 15:23:43 +0530

    move vim stuff into common

--- setup.sh
+++ setup.sh
@@ -15,8 +15,8 @@ ln -s `find $cwd/{$src,common}/gitconfig 2>/dev/null | head -1` ~/.gitconfig
 mkdir -p ~/.ssh && ln -s `find $cwd/{$src,common}/sshconfig 2>/dev/null | head -1` ~/.ssh/config
 mkdir -p ~/.ssh/cm_socket
 
-ln -s $cwd/vimrc ~/.vimrc
-ln -s $cwd/dotvim ~/.vim
+ln -s `find $cwd/{$src,common}/vimrc 2>/dev/null | head -1` ~/.vimrc
+ln -s `find $cwd/{$src,common}/dotvim -maxdepth 0 2>/dev/null | head -1` ~/.vim
 ln -s `find $cwd/{$src,common}/tmux.conf 2>/dev/null | head -1` ~/.tmux.conf
 
 rm -f ~/.zshrc && ln -s $cwd/$src/ohmyzsh/zshrc ~/.zshrc
