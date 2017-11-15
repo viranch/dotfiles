5408481673ab62bc4d98be96acaaa7ae0727e8ba
Viranch Mehta <email@viranch.me>
2017-11-15 12:38:38 +0530

    cuz imma an idiot

--- common/ohmyzsh/golang.zsh
+++ common/ohmyzsh/golang.zsh
@@ -1,9 +1,9 @@
 if [[ -d /usr/local/go ]]; then
     export PATH=$PATH:/usr/local/go/bin
-    export GOPATH=/Users/vimehta/code/go_path
+    export GOPATH=$HOME/code/go_path
 fi
 
 if [[ -d /usr/local/opt/go ]]; then
     export PATH=$PATH:/usr/local/opt/go/bin
-    export GOPATH=/Users/vimehta/code/go_path
+    export GOPATH=$HOME/code/go_path
 fi
