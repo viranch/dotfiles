10b729fa634d1363ebb37223dcda79b728dd678d
Viranch Mehta <email@viranch.me>
2018-02-24 21:24:34 +0530

    ubuntu zsh- add more aliases

--- ubuntu/ohmyzsh/aliases.zsh
+++ ubuntu/ohmyzsh/aliases.zsh
@@ -1,4 +1,21 @@
+# My useful aliases
+alias ns='sudo netstat -ntp'
+alias nsl='sudo netstat -ntlp'
+
+# systemctl
+alias sys='sudo systemctl'
+alias start='sys start'
+alias stop='sys stop'
+alias restart='sys restart'
+alias reload='sys reload'
+alias status='sys status'
+alias senable='sys enable'
+alias sdisable='sys disable'
+alias sys-reload='sys daemon-reload'
+
+# apt-get aliases
 alias ag='sudo apt-get'
 alias agi='ag install'
 alias agu='ag update'
+alias agr='ag remove --auto-remove'
 alias acs='apt-cache search'
