c15aeb02bf655914fc95efd559bbc9542b0eb029
Viranch Mehta <email@viranch.me>
2026-05-30 16:43:24 +0530

    Add mac homebrew stuff

---
+++ mac/ohmyzsh/homebrew.zsh
@@ -0,0 +1,6 @@
+export HOMEBREW_PREFIX="/opt/homebrew";
+export HOMEBREW_CELLAR="/opt/homebrew/Cellar";
+export HOMEBREW_REPOSITORY="/opt/homebrew";
+export PATH="/opt/homebrew/bin:/opt/homebrew/sbin:/usr/local/sbin:/usr/local/bin:/usr/bin:/usr/bin/site_perl:/usr/bin/vendor_perl:/usr/bin/core_perl:/home/neo/.dotfiles/bin:/home/neo/bin:/home/neo/.local/bin:/home/neo/.dotfiles/bin:/home/neo/bin:/home/neo/.local/bin:/home/neo/.dotfiles/bin:/home/neo/bin:/home/neo/.local/bin";
+export MANPATH="/opt/homebrew/share/man::";
+export INFOPATH="/opt/homebrew/share/info:";
